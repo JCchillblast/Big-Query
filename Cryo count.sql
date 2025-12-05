6e4f3cb08c6935ccf7b1b70da7ce525ad244d00b
JCchillblast <JamesCr@chillblast.com>
2025-12-05 17:40:10 +0000

    Refactor SQL query for date range and grouping
    Updated SQL query to include a date range filter and modified grouping logic for item IDs.

--- Cryo count.sql
+++ Cryo count.sql
@@ -4,18 +4,21 @@ WITH filtered_events AS (
     item__id,
     event__name
   FROM `ga_processed.ga4_data`
-  WHERE company = 'chillblast'
-    AND DATE = '2025-12-02'
-    AND item__id IN (
-      'CB-GAM-CORE-016',
-      'SPK4224',
-      'MOU3799',
-      'KBD2808',
-      'MON9690',
-      'MON9691',
-      'BUN0204',
-      'CB-GAM-CORE-016_CBB-CHILLBLAST'
-    )
+  WHERE DATE BETWEEN '2025-12-02' AND '2025-12-05'            -- partition filter required
+    AND company = 'chillblast'
+    AND (
+          item__id IN (
+            'CB-GAM-CORE-016',
+            'SPK4224',
+            'MOU3799',
+            'KBD2808',
+            'MON9690',
+            'MON9691',
+            'BUN0204',
+            'CB-GAM-CORE-016_CBB-CHILLBLAST'
+          )
+          OR item__id LIKE '%_CBB-CHILLBLAST'   -- matches ALL CBB variants
+        )
 ),
 
 atb_customers AS (
@@ -40,28 +43,35 @@ all_users AS (
   SELECT 
     u.user__id,
     u.item__id,
-    IF(a.user__id IS NOT NULL, TRUE, FALSE) AS atb,
+    IF(a.user__id IS NOT NULL, TRUE, FALSE) AS atb,       -- was there a match from Left Joins against the Union
     IF(b.user__id IS NOT NULL, TRUE, FALSE) AS attach,
     IF(p.user__id IS NOT NULL, TRUE, FALSE) AS purchase
   FROM (
     SELECT DISTINCT user__id, item__id FROM atb_customers
     UNION DISTINCT
     SELECT DISTINCT user__id, item__id FROM attach_customers
-    UNION DISTINCT
+    UNION DISTINCT                                                 -- each event line by line, multiple lines per user id in one table
     SELECT DISTINCT user__id, item__id FROM purchase_customers
   ) u
   LEFT JOIN atb_customers a USING (user__id, item__id)
   LEFT JOIN attach_customers b USING (user__id, item__id)
-  LEFT JOIN purchase_customers p USING (user__id, item__id)
+  LEFT JOIN purchase_customers p USING (user__id, item__id)            -- addds boolean colunns
 )
 
 SELECT
-  item__id,
-  COUNTIF(atb) AS atb_users,
-  COUNTIF(attach) AS attach_users,
+  CASE 
+    WHEN item__id LIKE '%_CBB-CHILLBLAST' THEN '_CBB-CHILLBLAST'
+    ELSE item__id
+  END AS item_group,
+  COUNTIF(attach) AS attach_total,   -- all attach clicks
+  COUNT(attach) - COUNTIF(attach AND atb) AS attach_pdp_session, 
+  COUNTIF(atb AND NOT attach) AS atb_pdp,
+  COUNTIF(attach AND atb) AS attach_atb,
   COUNTIF(purchase) AS purchasers_total,
   COUNTIF(purchase AND attach) AS purchasers_via_attach,
-  COUNTIF(purchase AND NOT attach) AS purchasers_via_pdp
+  COUNTIF(purchase AND NOT attach) AS purchasers_via_pdp,
+  ROUND(SAFE_DIVIDE(COUNTIF(attach AND atb) , COUNT(attach))*100, 2) AS atb_rate
 FROM all_users
-GROUP BY item__id
-ORDER BY item__id;
+GROUP BY item_group
+ORDER BY item_group;
+
